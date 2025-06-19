6cac752d38e64e4200d4e87098e2d062e3deb998
188mahin <mahinjparikh@gmail.com>
2025-06-19 16:05:01 +0530

    project

---
+++ lesson 1/Project- Check age.py
@@ -0,0 +1,10 @@
+age=int(input("enter the age of the user:"))
+if age>=10 and age<=20:
+    print("you are a teenager")
+else:
+    if age<10:
+        print("you are a kid ")
+    else:
+        print("you are an adult")
+
+        
