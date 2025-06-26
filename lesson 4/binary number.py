d1098b410d577bb5f4a260a0f7828f886882b46c
188mahin <mahinjparikh@gmail.com>
2025-06-26 10:52:52 +0530

    project

---
+++ lesson 4/binary number.py
@@ -0,0 +1,21 @@
+num=int(input("enter a number:"))
+n=0
+t=num
+binary=""
+while t>=1:
+    if t%2==0:
+        binary+='0'
+    else:
+        binary+='1'
+    n+=1
+    t/=2
+binary1=""
+for i in binary:
+    binary1=i+binary1
+print("the binary number for",num,"is",binary1)
+
+
+    
+
+    
+    
