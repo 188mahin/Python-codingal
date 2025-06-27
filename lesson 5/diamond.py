98fc550e9affa0946c1974a5d6e849de9e1d30c7
188mahin <mahinjparikh@gmail.com>
2025-06-27 11:55:18 +0530

    lesson5

--- lesson 5/diamond.py
+++ lesson 5/diamond.py
@@ -1,4 +1,4 @@
-rowsize=int(input("enter the number of rows"))
+rowsize=int(input("enter the number of rows:"))
 if rowsize%2==0:
     halfdiamrows=int(rowsize/2)
 else:
@@ -10,6 +10,16 @@ for i in range(1,halfdiamrows+1):
     space=space-1
     num=1
     for j in range(2*i-1):
+        print(end=str(num))
+        num+=1
+    print()
+space=1
+for i in range(1,halfdiamrows):
+    for j in range(1,space+1):
+        print(end=" ")
+    space=space+1
+    num=1
+    for j in range(1,2*(halfdiamrows-i)):
         print(end=str(num))
         num+=1
     print()
